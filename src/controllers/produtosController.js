9e95e43ea9b7696898d7a65852f81407e0206dd0
Bruna Bento <brunabento016@gmail.com>
2026-03-09 17:36:27 -0300

    feat(produtos): cria estrutura inicial do controller com funções vazias

---
+++ src/controllers/produtosController.js
@@ -0,0 +1,32 @@
+// armazenamento em memória
+let produtos = [];
+let nextId = 1;
+
+// listar todos os produtos
+function listar(req, res) {
+}
+
+// buscar produto por ID
+function buscarPorId(req, res) {
+}
+
+// criar novo produto
+function criar(req, res) {
+
+}
+
+// atualizar produto
+function atualizar(req, res) {
+}
+
+// remover produto
+function remover(req, res) {
+}
+
+module.exports = {
+    listar,
+    buscarPorId,
+    criar,
+    atualizar,
+    remover
+};
